8b220e160a9d39198fa467f3671b36a1babcf0e6
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-04-22 18:32:31 +0500

    add Create table status

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import sqlite3
 import telebot
 
+conn = sqlite3.connect("zero_order_service.db")
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS Status (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL)""")
+
+conn.commit()
+conn.close()
+
