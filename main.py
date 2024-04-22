2dc179db36ec652f7f04e25e72595ae60d3989dc
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-04-22 19:17:40 +0300

    (new3_users) main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import sqlite3
-conn = sqlite3.connect('Users.db')
-cur = conn.cursor()
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS User_role
-(id INTEGER PRIMARY KEY,
-name STRING UNIQUE)
-''')
+def create_tables():
+    conn = sqlite3.connect('Users.db')
+    cur = conn.cursor()
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS users
-(id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-phone STRING,
-address STRING,
-sum_of_orders FLOAT,
-discount FLOAT,
-user_role STRING,
-FOREIGN KEY(user_role) REFERENCES User_role(name))
-''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    phone STRING,
+    address STRING,
+    sum_of_orders FLOAT,
+    discount FLOAT,
+    user_role INTEGER,
+    FOREIGN KEY(user_role) REFERENCES User_role(id))
+    ''')
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
+
+# Вызываем функцию
+create_tables()
+
