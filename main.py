415945a290585c83ff6af9c1861b1fadf3e2c99b
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-04-22 18:41:47 +0300

    (new2_users) main.py

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ conn = sqlite3.connect('Users.db')
 cur = conn.cursor()
 
 cur.execute('''
+CREATE TABLE IF NOT EXISTS User_role
+(id INTEGER PRIMARY KEY,
+name STRING UNIQUE)
+''')
+
+cur.execute('''
 CREATE TABLE IF NOT EXISTS users
 (id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
 phone STRING,
 address STRING,
 sum_of_orders FLOAT,
-discount FLOAT)
+discount FLOAT,
+user_role STRING,
+FOREIGN KEY(user_role) REFERENCES User_role(name))
 ''')
 
 conn.commit()
