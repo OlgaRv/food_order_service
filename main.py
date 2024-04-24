c9bfaf646a36066d650aad22731627d4a01c7b9f
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-04-24 20:42:01 +0500

    создание таблицы юзер создание таблицы категории

--- main.py
+++ main.py
@@ -85,3 +85,30 @@ def create_table_dishes():
     conn.commit()
     conn.close()
 
+def create_table_users():
+    conn = sqlite3.connect('zero_order_service.db')
+    cur = conn.cursor()
+    cur.execute('''    CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    phone STRING,    address STRING,
+    sum_of_orders FLOAT,    discount FLOAT,
+    user_role INTEGER,
+    FOREIGN KEY(user_role) REFERENCES User_role(id))
+    ''')
+    conn.commit()
+    conn.close()
+
+    def create_table_category():
+        # Создаём базу данных
+        conn = sqlite3.connect('zero_order_service.db')
+        cursor = conn.cursor()
+        # Создаем таблицу
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Category(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category_name TEXT NOT NULL)
+        ''')
+        # Закрываем соединение с базой данных
+        conn.commit()
+        conn.close()
